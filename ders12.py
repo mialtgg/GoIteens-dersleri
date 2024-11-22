80679f02a6418e1f6806fca828e97ab143103c52
Mine ALTUĞ <99974782+mialtgg@users.noreply.github.com>
2024-11-22 14:40:22 +0300

    ders 12

---
+++ ders12.py
@@ -0,0 +1,7 @@
+telefon_rehberi = {
+    "Ali": "123-4567",
+    "Veli": "234-5678",
+    "Ayşe": "345-6789"
+}
+#items() Metodu: Bu metod, sözlükteki her anahtar-değer çiftini bir tuple (demet) olarak döndürür. Böylece her anahtar ve değeri ayrı ayrı işleyebilirsiniz.
+print(telefon_rehberi.items())
